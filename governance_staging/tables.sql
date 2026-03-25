2d128c0d2784065fb269cd7612b8a28333626417
pavanjangam98 <jpavan98@gmail.com>
2026-03-25 09:34:23 +0530

    Update tables.sql

--- governance_staging/tables.sql
+++ governance_staging/tables.sql
@@ -221,3 +221,46 @@ incremental_filter AS (
 SELECT *
 FROM incremental_filter
 +++++++++++++++++++++++++++++++++++
+{{ config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='catalog_set_property_id',
+    on_schema_change='sync_all_columns'
+) }}
+
+WITH source_data AS (
+    SELECT
+        *,
+        CURRENT_TIMESTAMP AS LOADED_AT
+    FROM {{ source('alation_share', 'catalog_set_membership') }}
+),
+
+incremental_filter AS (
+    SELECT *
+    FROM source_data
+    {% if is_incremental() %}
+    WHERE GREATEST(
+        COALESCE(TS_UPDATED, '1900-01-01'),
+        COALESCE(TS_DELETED, '1900-01-01')
+    ) >
+    (
+        SELECT COALESCE(
+            DATEADD(day, -1,
+                MAX(
+                    GREATEST(
+                        COALESCE(TS_UPDATED, '1900-01-01'),
+                        COALESCE(TS_DELETED, '1900-01-01')
+                    )
+                )
+            ),
+            '1900-01-01'
+        )
+        FROM {{ this }}
+    )
+    {% endif %}
+)
+
+SELECT *
+FROM incremental_filter
++++++++++++++++++
+
