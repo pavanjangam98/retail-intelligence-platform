3a34f20d429d909a6ba98170214d3761bdaace3d
pavanjangam98 <jpavan98@gmail.com>
2026-03-27 15:38:11 +0530

    Update scmcut_dag.py

--- governance_staging/scmcut_dag.py
+++ governance_staging/scmcut_dag.py
@@ -46,3 +46,41 @@ def scmcust_foundation_fdp_custmstr_fdp__party_address():
 
 # ✅ Fixed: DAG instantiation call
 scmcust_foundation_fdp_custmstr_fdp__party_address()
+
+++++++++++++++
+
+
+import os
+from datetime import datetime
+from airflow.decorators import dag
+from cosmos.operators import DbtRunOperator
+from cosmos import ProfileConfig
+from cosmos.profiles import SnowflakePrivateKeyPemProfileMapping
+
+MODEL_NAME = "foundation___fdp__custmstr___fdp__party_address"
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name="dev",
+    profile_mapping=SnowflakePrivateKeyPemProfileMapping(
+        conn_id="sfconnect",
+    ),
+)
+
+@dag(
+    dag_id="scmcust_foundation_fdp_custmstr_fdp__party_address",
+    start_date=datetime(2025, 1, 1),
+    schedule=None,
+    catchup=False,
+)
+def scmcust_foundation_fdp_custmstr_fdp__party_address():
+
+    DbtRunOperator(
+        task_id="run_model",
+        project_dir="/usr/local/airflow/repo/dags/bdh_customer_dbt/",
+        profile_config=profile_config,
+        select=MODEL_NAME,
+        dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
+    )
+
+dag = scmcust_foundation_fdp_custmstr_fdp__party_address()
